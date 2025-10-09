71ec699dbdcab7b78b8e09889d4d282ac3ad29a1
atrebit <atrebit@users.noreply.github.com>
2025-10-10 00:20:11 +0200

    move scheduler functionality

--- app/api/outages/route.ts
+++ app/api/outages/route.ts
@@ -1,32 +1,9 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/utils/db";
-import { Prisma, OutageStatus } from "@prisma/client";
-import axios, { AxiosResponse } from 'axios';
 
 export async function GET() {
-//  const outages = await prisma.serverOutage.findMany();
-  axios.get('http://localhost/outages') // get request auf localhost/outages (JSON-Server antwortet)
-  .then(async function (response) { // im Falle einer positiven Antwort
-    await prisma.serverOutage.createMany({ // schreiben wir die Daten in auf die DB
-      data: response.data,
-    });// handle success
-  })
-  .catch(function (error) { // im Falle eines Fehlers beim Anfragen der Daten des JSON-Servers (z.B. Server nicht erreichbar), dann
-    // handle error
-    console.log(error); // Fehler in der Konsole ausgeben
-  });
-
-  axios.get('http://localhost/updates') // get request auf localhost/outages (JSON-Server antwortet)
-  .then(async function (response) { // im Falle einer positiven Antwort
-    await prisma.updateSet.createMany({ // schreiben wir die Daten in auf die DB
-      data: response.data,
-    });// handle success
-  })
-  .catch(function (error) { // im Falle eines Fehlers beim Anfragen der Daten des JSON-Servers (z.B. Server nicht erreichbar), dann
-    // handle error
-    console.log(error); // Fehler in der Konsole ausgeben
-  });
-
+  const outages = await prisma.serverOutage.findMany();
+  console.log("Outages fetched:", outages);
   return new NextResponse(null, { status: 200 }); // und geben die Daten als JSON-Antwort zurück
 }
 
--- src/lib/scheduler/index.ts
+++ src/lib/scheduler/index.ts
@@ -1,8 +1,31 @@
 import cron from "node-cron";
-
+import { prisma } from "@/utils/db";
+import axios from 'axios';
 
 export function initScheduler() {
   cron.schedule("*/30 * * * * *", () => {
-    console.log("Task runs every hour");
+      console.log("Requesting and storing data from UpStream-Service...");
+      axios.get('http://localhost/outages') // get request auf localhost/outages (JSON-Server antwortet)
+      .then(async function (response) { // im Falle einer positiven Antwort
+        await prisma.serverOutage.createMany({ // schreiben wir die Daten in auf die DB
+          data: response.data,
+        });// handle success
+      })
+      .catch(function (error) { // im Falle eines Fehlers beim Anfragen der Daten des JSON-Servers (z.B. Server nicht erreichbar), dann
+        // handle error
+        console.log(error); // Fehler in der Konsole ausgeben
+      });
+    
+      axios.get('http://localhost/updates') // get request auf localhost/outages (JSON-Server antwortet)
+      .then(async function (response) { // im Falle einer positiven Antwort
+        await prisma.updateSet.createMany({ // schreiben wir die Daten in auf die DB
+          data: response.data,
+        });// handle success
+      })
+      .catch(function (error) { // im Falle eines Fehlers beim Anfragen der Daten des JSON-Servers (z.B. Server nicht erreichbar), dann
+        // handle error
+        console.log(error); // Fehler in der Konsole ausgeben
+      });
+    
   });
 }
