a34660e9d765a39c220e617aa4260d65c9dc43cb
atrebit <atrebit@users.noreply.github.com>
2025-10-20 19:06:25 +0200

    upstream-server-url is configurable

--- src/lib/scheduler/index.ts
+++ src/lib/scheduler/index.ts
@@ -2,6 +2,12 @@ import cron from "node-cron";
 import { prisma } from "@/utils/db";
 import axios from 'axios';
 
+const baseUrl = process.env.JSON_SERVER_URL;
+
+if (!baseUrl) {
+  throw new Error("Error: Environment variable in 'UPSTREAM_SERVER_URL' is not defined.");
+}
+
 export function initScheduler() {
   cron.schedule("5 * * * * *", () => {
       console.log("Requesting and storing data from UpStream-Service...");
@@ -15,8 +21,8 @@ export function initScheduler() {
         // handle error
         console.log(error); // Fehler in der Konsole ausgeben
       });
-    
-      axios.get('http://json-server/updates') // get request auf localhost/outages (JSON-Server antwortet)
+
+      axios.get('`${baseUrl}/updates`) // get request auf localhost/outages (JSON-Server antwortet)
       .then(async function (response) { // im Falle einer positiven Antwort
         await prisma.updateSet.createMany({ // schreiben wir die Daten in auf die DB
           data: response.data,
