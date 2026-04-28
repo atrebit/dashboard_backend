c59d47c9148a304eff33a328a7b8384a17c82b46
atrebit <atrebit@users.noreply.github.com>
2026-04-28 18:09:26 +0200

    add comments

--- src/lib/scheduler/index.ts
+++ src/lib/scheduler/index.ts
@@ -15,17 +15,17 @@ if (!baseUrl) {
 export function initScheduler() {
   cron.schedule("5 * * * * *", () => {
       console.log("Requesting and storing data from UpStream-Service...");
-      axios.get(`${baseUrl}/outages`) 
+      axios.get(`${baseUrl}/outages`)   // Abfrage des UpStream-Services nach Ausfall-Daten
       .then(async function (response) { 
-        await prisma.serverOutage.createMany({ 
+        await prisma.serverOutage.createMany({    // Speichern der erhaltenen Daten in der Datenbank
           data: response.data,
         });
       })
       .catch(function (error) { 
-        console.log(error); 
+        console.log(error);     // Loggen von potenziellen Fehlern
       });
 
-      axios.get(`${baseUrl}/updates`) 
+      axios.get(`${baseUrl}/updates`)   // Abfrage des UpStream-Services nach Update-Daten
       .then(async function (response) { 
         await prisma.updateSet.createMany({ 
           data: response.data,
